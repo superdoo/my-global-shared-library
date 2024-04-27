5562ad5aa6aef37b19b191690b3d1a3669c2a6e7
Superdoo <wbbmb2016@gmail.com>
2024-04-27 14:27:34 -0500

    ok

--- vars/currentClock.groovy
+++ vars/currentClock.groovy
@@ -1,14 +1,12 @@
+// GetDate.groovy doovy
+
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 
-// Get current date and time
-LocalDateTime currentDateTime = LocalDateTime.now()
-
-// Define a format for date and time
-DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")
-
-// Format the date and time using the defined formatter
-String formattedDateTime = currentDateTime.format(formatter)
-
-// Print the formatted date and time
-println "Current date and time: " + formattedDateTime
+class DateUtils {
+    static String getCurrentDateTime() {
+        LocalDateTime currentDateTime = LocalDateTime.now()
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")
+        return currentDateTime.format(formatter)
+    }
+}
