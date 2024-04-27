0fba7c1ca7f864a059850ab4a9ee2e1329bb547b
Superdoo <wbbmb2016@gmail.com>
2024-04-27 11:11:16 -0500

    fix message

--- vars/helloWorld.groovy
+++ vars/helloWorld.groovy
@@ -1,4 +1,4 @@
-def call(String name, String dayOfWeek ,string message)
+def call(String name, String dayOfWeek ,String message)
 {
     sh "echo Hello ${name}.Today is ${dayOfWeek}. Message of the day is ${message}"
     }
