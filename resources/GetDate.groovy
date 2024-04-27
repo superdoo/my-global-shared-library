945d699b242a6806ba064d71d2dcae5c97d29f4c
Superdoo <wbbmb2016@gmail.com>
2024-04-27 14:58:38 -0500

    ok

---
+++ resources/GetDate.groovy
@@ -0,0 +1,12 @@
+// GetDate.groovy doovy
+
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+
+class DateUtils {
+    static String getCurrentDateTime() {
+        LocalDateTime currentDateTime = LocalDateTime.now()
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")
+        return currentDateTime.format(formatter)
+    }
+}
