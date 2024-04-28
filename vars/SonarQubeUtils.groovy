9a5d9f4092138f260415b5cbe023214f70ff271c
Superdoo <wbbmb2016@gmail.com>
2024-04-27 23:30:17 -0500

    ok

--- vars/SonarQubeUtils.groovy
+++ vars/SonarQubeUtils.groovy
@@ -1,12 +1,9 @@
 // SonarQubeUtils.groovy
 
 def callSonarQubeScanner(String projectName, String projectKey) {
-    // Call SonarQube scanner
-    withSonarQubeEnv() {
-      sh "${scannerHome}/bin/sonar-scanner"
-    }
-    //withSonarQubeEnv('SonarQube') {
-      //  sh "mvn sonar:sonar -Dsonar.projectKey=${projectKey} -Dsonar.projectName=${projectName}"
+     Call SonarQube scanner
+       withSonarQubeEnv('SonarQube') {
+       sh "sonar:sonar -Dsonar.projectKey=${projectKey} -Dsonar.projectName=${projectName}"
     }
 
 
